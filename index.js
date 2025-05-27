35584fa0bb4f8531a74ff9978b4e669b943050b5
riomanik <rionaldimanik999@gmail.com>
2025-05-27 13:46:18 +0700

    [Update] Update URL

--- index.js
+++ index.js
@@ -17,25 +17,55 @@ function formatToRupiah(amount) {
   }).format(amount);
 }
 
-async function checkPrice(channel) {
-  try {
-    const { data } = await axios.get('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=idr');
-    const priceIdr = data.bitcoin.idr;
-    const formatted = formatToRupiah(priceIdr);
+// async function checkPrice(channel) {
+//   try {
+//     const { data } = await axios.get('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=idr');
+//     const priceIdr = data.bitcoin.idr;
+//     const formatted = formatToRupiah(priceIdr);
 
-    console.log(`[LOG] Harga BTC: ${formatted}`);
+//     console.log(`[LOG] Harga BTC: ${formatted}`);
 
-    if (priceIdr > 1800000000) {
-      await channel.send(`🚀 Harga BTC NAIK! Sekarang: ${formatted}`);
-    } else if (priceIdr <= 1720000000) {
-      await channel.send(`🔻 Harga BTC TURUN! Sekarang: ${formatted}`);
-    } else {
-      await channel.send(`📈 Harga BTC: ${formatted}`);
+//     if (priceIdr > 1800000000) {
+//       await channel.send(`🚀 Harga BTC NAIK! Sekarang: ${formatted}`);
+//     } else if (priceIdr <= 1720000000) {
+//       await channel.send(`🔻 Harga BTC TURUN! Sekarang: ${formatted}`);
+//     } else {
+//       await channel.send(`📈 Harga BTC: ${formatted}`);
+//     }
+
+//   } catch (err) {
+//     console.error('Gagal ambil harga:', err.message);
+//   }
+// }
+
+const coins = [
+  'btc_idr',
+  'eth_idr',
+  'usdt_idr',
+  'alpaca_idr',
+  'hedg_idr',
+  'pepe_idr',
+  'moodeng_idr',
+  'h2o_idr'
+];
+
+async function fetchPrices() {
+  const prices = {};
+
+  for (const coin of coins) {
+    try {
+      const res = await axios.get(`https://indodax.com/api/${coin}/ticker`);
+      prices[coin] = parseFloat(res.data.ticker.last);
+    } catch (err) {
+      console.error(`Gagal fetch harga ${coin}:`, err.message);
     }
-
-  } catch (err) {
-    console.error('Gagal ambil harga:', err.message);
   }
+
+  let message = '📊 Harga crypto dari Indodax:\n';
+  for (const [coin, price] of Object.entries(prices)) {
+    message += `${coin.toUpperCase().replace('_IDR','')}: Rp${price.toLocaleString()}\n`;
+  }
+  await channel.send(message);
 }
 
 client.once('ready', async () => {
@@ -48,9 +78,8 @@ client.once('ready', async () => {
     return;
   }
 
-  await checkPrice(channel);
-
-  setInterval(() => checkPrice(channel), 3600000);
+  await fetchPrices(channel);
+  setInterval(() => fetchPrices(channel), 3600000);
 });
 
 client.login(TOKEN);
